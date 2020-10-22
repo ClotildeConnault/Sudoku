b0e281d1aa6373eb363e0cf2404139570046de34
Clotilde Connault <clotilde.connault@gmail.com>
2020-10-22 14:38:53 +0200

    ajout classe Possibilities

---
+++ sudoku2/src/be/technifutur/java2020/sudoku/commun/Possibilities.java
@@ -0,0 +1,53 @@
+package be.technifutur.java2020.sudoku.commun;
+
+public class Possibilities {
+    private int data;
+
+    public boolean add(int val){
+        boolean change = false;
+        int updatedData = data | (1<<val-1);
+
+        if ((data ^ updatedData) != 0){
+            data = updatedData;
+            change = true;
+        }
+
+        return change;
+    }
+
+    public boolean remove(int val){
+        boolean change = false;
+        int updatedData = data & (~1<<val-1);
+
+        if ((data ^ updatedData) != 0){
+            data = updatedData;
+            change = true;
+        }
+
+        return change;
+    }
+
+    public boolean contains(int val){
+        boolean contains = false;
+
+        if ((data & (1<<val-1)) != 0){
+            contains = true;
+        }
+
+        return contains;
+    }
+
+    public int size(){
+        int possibilities = 0;
+
+        for(int i=1; i <= 9; i ++){
+            if ((data & (1<<i-1)) == 0){
+                possibilities ++;
+            }
+        }
+
+        return possibilities;
+    }
+
+
+}
