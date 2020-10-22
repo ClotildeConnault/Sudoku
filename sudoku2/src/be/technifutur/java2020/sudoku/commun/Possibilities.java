fc79aa34612ec000bfcd31ba6c162c76d0956827
Clotilde Connault <clotilde.connault@gmail.com>
2020-10-22 14:49:58 +0200

    simplification add (possibilities)

--- sudoku2/src/be/technifutur/java2020/sudoku/commun/Possibilities.java
+++ sudoku2/src/be/technifutur/java2020/sudoku/commun/Possibilities.java
@@ -7,7 +7,7 @@ public class Possibilities {
         boolean change = false;
         int updatedData = data | (1<<val-1);
 
-        if ((data ^ updatedData) != 0){
+        if (data != updatedData){
             data = updatedData;
             change = true;
         }
